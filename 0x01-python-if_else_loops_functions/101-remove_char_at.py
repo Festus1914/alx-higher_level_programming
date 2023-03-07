9898a0413ee5e5f4385aab35cc845c21199a0d47
Festus1914 <oladiplentyfestus@gmail.com>
2023-03-07 05:17:27 -0800

    101-remove_char_at.py

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-# Author - Oladipupo Oladotun Festus
+# Author -  Oladipupo Oladotun Festus
 
 def remove_char_at(str, n):
     if n < 0:
         return (str)
     return (str[:n] + str[n+1:])
-
