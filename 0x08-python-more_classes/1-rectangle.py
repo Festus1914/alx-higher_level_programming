36f172f2e0d849b2ee94c6ade2553a14d0a4390b
Festus1914 <oladiplentyfestus@gmail.com>
2023-04-03 05:15:13 -0700

    Class that defines a Rectangle

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+class Rectangle:
+    """Creating a Rectangle with a width and height"""
+    def __init__(self, w, h):
+
+        self.width = w
+        self.height = h
+
+        def area(self):
+            return self.width * self.height
