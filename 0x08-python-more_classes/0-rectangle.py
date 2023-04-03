534c651fa861a8266f74c6018d2aa0eab9551ff1
Festus1914 <oladiplentyfestus@gmail.com>
2023-04-03 04:50:55 -0700

    Creating an empty Rectangle

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
+"""Creating an empty Rectangle"""
 
-"""I will write an empty class Rectangle that defines a rectangle"""
+
 
 class Rectangle:
-
-    """Create an empty Rectangle object"""
-    passed
+""" Subtitute a Rectangle"""
+passed
