2c815c9f9f3e41ec1bd8e3c28973e4c01c131a11
Festus1914 <oladiplentyfestus@gmail.com>
2023-04-11 08:11:51 -0700

    returns a list object

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" Functions that returns the list of available attributes
+    and methods of an object
+"""
+
+
+def lookup(obj):
+    """
+    returns a list object
+    """
+    return(obj)
