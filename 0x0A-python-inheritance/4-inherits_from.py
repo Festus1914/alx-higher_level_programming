03c90e132986fc7af6e8dfa300b3071de50873a0
Festus1914 <oladiplentyfestus@gmail.com>
2023-04-11 09:17:29 -0700

    4-inherits_from.py

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Write a function that returns Tru
+e if the object is an instance of a class."""
+
+
+
+def inherits_from(obj, a_class):
+
+    """
+    if the object is an instance of a class that inherited
+    (directly or indirectly) from the specified class.
+    """
+
+    return isinstance(obj, a_class) and type(obj) != a_class
