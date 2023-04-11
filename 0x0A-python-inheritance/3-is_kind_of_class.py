fd786368776077aa2c5386db66d8f40a47196759
Festus1914 <oladiplentyfestus@gmail.com>
2023-04-11 09:09:34 -0700

    3-is_kind_of_class.py

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -6,11 +6,9 @@ Defines a function of a conditional
 
 
 def is_kind_of_class(obj, a_class):
-    """
-    Our function = is_kind_of_class
+    """ Our function = is_kind_of_class
     Our object = "obj"
     a_class = "class"
     Return =  boolean values
     """
-
     return isinstance(obj, a_class)
