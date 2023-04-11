c3d71db23a950efda3c09c348856ba38628edd1b
Festus1914 <oladiplentyfestus@gmail.com>
2023-04-11 09:52:03 -0700

    7-base_geometry.py

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Defines a base geometry class BaseGeometry."""
+
+
+
+
+class BaseGeometry:
+    """Represent base geometry."""
+
+
+    def area(self):
+        """Not implimented."""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Creating a function integer_validator"""
+        self.name = name
+        self.value = value
+
+        """
+            Creating a conditional statements
+
+        """
+        if type value != int:
+            raise TypeError(name + " must be an integer")
+        if type value <= 0:
+            raise ValueError(name + "must be greater than 0")
