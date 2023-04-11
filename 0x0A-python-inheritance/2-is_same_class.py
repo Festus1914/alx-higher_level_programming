5ddb693ea8969ff94bf5bb2733effc01fdf0a827
Festus1914 <oladiplentyfestus@gmail.com>
2023-04-11 08:50:45 -0700

    2-is_same_class.py

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
+"""Define a class function"""
+
+
 def is_same_class(obj, a_class):
+    """Creating a conditional of either True or False"""
+
     if type(obj) == a_class:
+        """
+        Returning the booleans
+        """
         return True
     return False
