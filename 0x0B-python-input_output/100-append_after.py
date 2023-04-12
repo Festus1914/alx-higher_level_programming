d498774905491e433ea1bbf9161c4085733d828c
Festus1914 <oladiplentyfestus@gmail.com>
2023-04-12 10:36:41 -0700

    100-append_after.py

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+'''  Function that inserts a line of text to a file, after each line containing a specific string
+'''
+
+
+def append_after(filename="", search_string="", new_string=""):
+    ''' Function search and update.
+    '''
+    with open(filename, 'r+') as f:
+        lines = f.readlines()
+        i = 0
+        for line in lines:
+            if line.find(search_string) is not -1:
+                lines.insert(i + 1, new_sting)
+            i += 1
+        f.seek(0)
+        f.write("".join(lines))
+
