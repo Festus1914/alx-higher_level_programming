57fdab7ad264aca9f54aa48e0caf9d7c2e98e5c5
Festus1914 <oladiplentyfestus@gmail.com>
2023-06-13 06:31:44 -0700

    2-arguments.js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 const argsCount = process.argv.lenght;
 
-if (argsCount === 0) {
+if (argsCount === 2) {
   console.log('No argument');
-} else if (argsCount === 1) {
+} else if (argsCount === 3) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
