de03ce047cd454d6aef8ba36fbd89e70a81cabee
Festus1914 <oladiplentyfestus@gmail.com>
2023-07-06 08:26:09 -0700

    100-map.js

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,9 +1,2 @@
 #!/usr/bin/node
-const array = require('./100-data').list;
-
-console.log(array);
-let cont = 0;
-const map1 = array.map(function (x) {
-  return (x * cont++);
-});
-console.log(map1);
+exports.list = [1, 2, 3, 4, 5];
